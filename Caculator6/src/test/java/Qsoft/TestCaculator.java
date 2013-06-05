c1de53ed53a599bc2b9f3c676bbf5c4a03f7bacc
Phạm Trần Trung <trungptdhcn@gmail.com>
2013-06-05 22:04:22 +0700

    00

---
+++ Caculator6/src/test/java/Qsoft/TestCaculator.java
@@ -0,0 +1,17 @@
+package Qsoft;
+
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Trung
+ * Date: 05/06/2013
+ * Time: 22:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestCaculator extends TestCase {
+    public void testStringEmpty()
+    {
+        assertEquals(0,Caculator.add(""));
+    }
+}
