1874b45979e7a1e6b327530b7cb96b5da45bdc85
Phạm Trần Trung <trungptdhcn@gmail.com>
2013-06-13 15:48:22 +0700

    04 Red

--- Banking2/src/main/java/Transaction.java
+++ Banking2/src/main/java/Transaction.java
@@ -13,7 +13,6 @@ public class Transaction {
     }
 
     public static void doDeposit(String accountNumber, long timeStamp, double amount, String description) {
-        TransactionDTO mTransactionDTO = new TransactionDTO(accountNumber,timeStamp,amount,description);
-        mTransactionDAO.save(mTransactionDTO);
+
     }
 }
